cee97ce8e4453aa2c49ab0e56170c73a8305f758
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-18 17:20:14 -0500

    create conetion db

---
+++ dataBase/dataBase.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine, MetaData
+
+engineBD = create_engine("mysql+pymysql://root@12345678@localhost:3306/markbotdb")
+
+metaData = MetaData()
+
+conetion = engineBD.connect()
