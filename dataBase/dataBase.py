4f5deeba9e17692022bc069260dc7d74c7e937c5
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-19 15:09:55 -0500

    change conetion

--- dataBase/dataBase.py
+++ dataBase/dataBase.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engineBD = create_engine("mysql+pymysql://root@12345678@localhost:3306/markbotdb")
+engineBD = create_engine("mysql+pymysql://root:12345678@localhost:3306/markbotdb")
 
 metaData = MetaData()
 
