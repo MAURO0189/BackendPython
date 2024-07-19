4e3bcb71dc761f83d968846fd0c80c150656d8bb
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-19 17:21:09 -0500

    add config cors

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
+
 from routes.routesAPI import routes
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routes)
