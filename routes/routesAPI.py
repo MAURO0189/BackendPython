c3c6df49a82877b62b510179e18cc2bea92a55e7
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2024-07-20 10:18:59 -0500

    add new route and new answers

--- routes/routesAPI.py
+++ routes/routesAPI.py
@@ -1,10 +1,38 @@
 from fastapi import APIRouter, HTTPException
+
+from pydantic import BaseModel
+
 from sqlalchemy import select 
+
+from sqlalchemy.sql import func
 
 from models.chatBot import chat
 from dataBase.dataBase import conetion 
 
+class ChatRequest(BaseModel):
+    pregunta: str
+
 routes = APIRouter()
+
+@routes.post("/markbot")
+def consultarChat(request: ChatRequest):
+    try:
+        normalizada_pregunta = request.pregunta.strip().lower()
+        consulta = select(chat).where(func.lower(func.trim(chat.c.pregunta)) == normalizada_pregunta)
+        res = conetion.execute(consulta).fetchone()
+        if res:
+            return {"respuesta": res.respuesta}
+        else:
+            respuesta_default = (
+                "Lo siento, no tengo una respuesta para esa pregunta, pero tienes estos enlaces que te pueden ayudar a resolver tus inquietudes:<br>"
+                "- <a href='https://www.segurossura.com.co/paginas/movilidad/autos/centros-de-servicio/inicio.aspx' target='_blank' rel='noopener noreferrer'>Centro de Servicios Autos SURA</a><br>"
+                "- <a href='https://www.epssura.com/' target='_blank' rel='noopener noreferrer'>EPS SURA</a><br>"
+                "- <a href='https://www.segurossura.com.co/paginas/inicio.aspx' target='_blank' rel='noopener noreferrer'>Seguros SURA</a>"
+            )
+            return {"respuesta": respuesta_default}
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
+
 
 @routes.get("/markbot")
 def consultarChat():
@@ -16,3 +44,8 @@ def consultarChat():
 
     except Exception as error:
         raise HTTPException(status_code=500, detail=str(error))
+    
+
+    
+
+    
